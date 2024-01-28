fe5bd17f866f852e0e2a19e398ebf6458c2f2fcd
CrisIntriago <caintria@espol.edu.ec>
2024-01-28 17:13:56 -0500

    Should be ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
     <header className="px-4 py-5 bg-white border-b">
         <div className="md:flex md:justify-between">
             <h2 className="text-4xl text-sky-600 font-black text-center mb-5 md:mb-0">
-                UpTask
+                ProProject
             </h2>
 
             <div className='flex flex-col md:flex-row items-center gap-4'>
@@ -27,17 +27,17 @@ const Header = () => {
                     type="button"
                     className='font-bold uppercase'
                     onClick={handleBuscador}
-                >Buscar Proyecto</button>
+                >Search Project</button>
                 <Link
                     to="/proyectos"
                     className='font-bold uppercase'
-                >Proyectos</Link>
+                >Project</Link>
 
                 <button
                     type="button"
                     className='text-white text-sm bg-sky-600 p-3 rounded-md uppercase font-bold'
                     onClick={handleCerrarSesion}
-                >Cerrar Sesión</button>
+                >Log Out</button>
 
                 <Busqueda />
             </div>
--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -45,8 +45,8 @@ const Login = () => {
 
   return (
     <>
-        <h1 className="text-sky-600 font-black text-6xl capitalize">Inicia sesión y administra tus {''}
-            <span className="text-slate-700">proyectos</span>
+        <h1 className="text-sky-600 font-black text-6xl capitalize">Login and Administer your {''}
+            <span className="text-slate-700">Projects</span>
         </h1>
 
         {msg && <Alerta alerta={alerta } />}
